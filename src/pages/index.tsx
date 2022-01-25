401f300aa229f681b07bb595a239fa1fce2f8cd9
Jean Martins <jeanmrtns4@gmail.com>
2022-01-25 12:56:09 -0300

    UserState and Submit form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,45 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import * as S from '../styles/HomeStyle';
+
+interface User {
+  nickname: string;
+}
 
 export default function Home(): JSX.Element {
 
-  const user = {
+  const [user, setUser] = useState<User>({
     nickname: 'jeanmrtns',
-    url: 'https://github.com/jeanmrtns.png',
-    name: 'Jean Martins',
-  };
+  });
+
+  const router = useRouter();
+
+  function handleChangeUser(e: ChangeEvent<HTMLInputElement>) {
+    setUser({nickname: e.target.value});
+  }
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    router.push('/chat');
+  }
 
   return (
     <S.Container>
       <S.Content>
-        <S.Form>
+        <S.Form onSubmit={handleSubmit}>
           <h2>Boas vindas a Vila da Folha</h2>
           <h3>Aluracord - Alura Naruto</h3>
-          <input type="text" value={user.nickname} />
+          <input type="text" value={user.nickname} onChange={handleChangeUser} placeholder='Seu usuário no Github' />
           <button>Entrar</button>
         </S.Form>
         <S.GitHubAvatar>
-          <Image src={user.url} alt={user.name} width={200} height={200} />
-          <span>{user.nickname}</span>
+          { user.nickname && (
+            <>
+              <Image src={`https://github.com/${user.nickname}.png`} alt={user.nickname} width={200} height={200} />
+              <span>{user.nickname}</span>
+            </>
+          )}
         </S.GitHubAvatar>
       </S.Content>
     </S.Container>
