2b62e720216dd6f90e33a752f26f08f1cac10bcd
Jean Martins <jeanmrtns4@gmail.com>
2022-01-27 02:52:55 -0300

    Offline chat

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,5 +1,105 @@
-export default function Chat() {
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import * as S from "../styles/ChatStyle";
+
+interface Message {
+    id: number;
+    user: string;
+    userName: string;
+    message: string;
+    createdAt: string;
+}
+
+const messages = [
+    {
+        id: 1,
+        user: 'jeanmrtns',
+        userName: 'Jean Martins',
+        message: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste accusantium, animi impedit voluptates cumque voluptatum ut! Vitae necessitatibus quibusdam quisquam fuga aspernatur eum magni, aliquam laborum. Iste veniam qui sequi.',
+        createdAt: '17/01/2021'
+    },
+    {
+        id: 2,
+        user: 'peas',
+        userName: 'Paulo Silveira',
+        message: 'Isso aqui foi muito legal',
+        createdAt: '17/01/2021'
+    },
+    {
+        id: 3,
+        user: 'jeanmrtns',
+        userName: 'Jean Martins',
+        message: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste accusantium, animi impedit voluptates cumque voluptatum ut! Vitae necessitatibus quibusdam quisquam fuga aspernatur eum magni, aliquam laborum. Iste veniam qui sequi.',
+        createdAt: '18/01/2021'
+    },
+]
+
+export default function Chat(): JSX.Element {
+
+    const [newMessage, setNewMessage] = useState('');
+    const [messagesList, setMessagesList] = useState<Message[]>(messages);
+
+    function handleChangeNewMessage(e: ChangeEvent<HTMLInputElement>) {
+        const message = e.target.value;
+        setNewMessage(message);
+    }
+
+    function handleSendNewMessage() {
+        setMessagesList([...messagesList, {
+            id: messagesList.length + 1,
+            createdAt: '27/01/2022',
+            message: newMessage,
+            user: 'jeanmrtns',
+            userName: 'Jean'
+        }]);
+
+        setNewMessage('');
+    }
+
     return (
-        <div>Chat</div>
+        <S.Container>
+            <Head>
+                <title>Aluracord - Naruto Chat</title>
+            </Head>
+            <S.Content onSubmit={e => e.preventDefault()}>
+                <S.Header>
+                    <h1>Chat</h1>
+                    <Link href="/"><a>Logout</a></Link>
+                </S.Header>
+
+                <S.Chat>
+                    { messagesList.map(message => {
+                        return (
+                            <S.Message key={message.id}>
+                                <div>
+                                    <Image src={`https://github.com/${message.user}.png`} alt={message.user} width={30} height={30} />
+                                    <h4>{message.userName}</h4>
+                                    <time>{message.createdAt}</time>
+                                </div>
+                                <p>{message.message}</p>
+                            </S.Message>
+                        )
+                    }) }
+                </S.Chat>
+
+                <S.Footer>
+                    <S.NewMessageInput 
+                        type="text" 
+                        value={newMessage} 
+                        onChange={handleChangeNewMessage} 
+                        placeholder="Digite sua mensagem"
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                e.preventDefault();
+                                handleSendNewMessage();
+                            }
+                        }}
+                    />
+                    <button onClick={handleSendNewMessage}>Enviar</button>
+                </S.Footer>
+            </S.Content>
+        </S.Container>
     );
 }
---
+++ src/styles/ChatStyle.ts
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    margin: 0 auto;
+    min-height: 100vh;    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Content = styled.form`
+    background-color: #fff;
+    padding: 1rem 2rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    border-radius: 0.25rem;
+    width: 90%;
+    height: 90vh;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    align-self: flex-start;
+    width: 100%;
+
+    a {
+        color: orange;
+    }
+`;
+
+export const Chat = styled.div`
+    background-color: #e7e7e3;
+    height: 100%;
+    width: 100%;
+    padding: 2rem;
+
+    overflow: scroll;
+`;
+
+export const Message = styled.div`
+
+    font-family: sans-serif;
+    padding: 1rem;
+
+    :not(:first-of-type) {
+        margin-top: 1rem;
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        margin-bottom: 0.25rem;
+
+        img {
+            border-radius: 50%;
+        }
+        
+        h4 {
+            margin-inline: 1rem;
+        }
+
+        time {
+            font-size: 0.5rem;
+            font-weight: bold;
+            color: #aaa;
+        }
+    }
+
+    &:hover {
+        background-color: #FFF;
+        border-radius: 0.25rem;
+    }
+
+`;
+
+export const Footer = styled.footer`
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    button {
+        height: 100%;
+        border: 1px solid orange;
+        background-color: inherit;
+        padding: 0 2rem;
+        cursor: pointer;
+        border-left: none;
+
+        &:hover {
+            background-color: orange;
+            color: #fff;
+        }
+    }
+`;
+
+export const NewMessageInput = styled.input`
+    display: block;
+    width: 100%;
+    padding: 1rem 1.25rem;
+    
+    border: 1px solid orange;
+    outline-color: orangered;
+    font-size: 1rem;
+`;
